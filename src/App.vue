1511ffbf1831521feb87d34df14665c0001f8e81
Alex <Alexlance13@gmail.com>
2019-06-29 02:41:38 +0800

    my second commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <form action class="m-2">Введите имя игрока:</form>
+    <input
+      class="m-2"
+      type="text"
+      v-model="player"
+      @keyup.enter="(!player.length) ? null : players.push(player); player=''"
+    >
+
+    <ul class="list-group">
+      <li class="list-group-item" v-for="player of players" :key="player">
+        <person :player="player" @close="removePlayer"></person>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Person from "./Person.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      player: "",
+      players: []
+    };
+  },
+  components: {
+    person: Person
+  },
+  methods: {
+    removePlayer(player) {
+      this.players.splice(this.players.indexOf(player), 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.m-2 {
+  display: inline-block;
+}
+</style>
---
+++ src/Person.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div>
+      {{player}}: level:
+      <span style="color: red">
+        <button @click="sub">-</button>
+        <strong>{{lvl}}</strong>
+        <button @click="add">+</button>
+      </span>
+      <span @click="sexBoolean = !sexBoolean">sex: {{sexBoolean ? 'F' : 'M'}}</span>
+      <span class="close" @click="close">x</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["player"],
+  data() {
+    return {
+      lvl: 1,
+      sexBoolean: false
+    };
+  },
+  methods: {
+    add() {
+      this.lvl++;
+      if (this.lvl === 10) {
+        alert(`${this.player} победил`);
+        location.reload();
+      }
+    },
+    sub() {
+      if (this.lvl === 1) return;
+      this.lvl--;
+    },
+    close() {
+      this.$emit("close", this.player);
+    }
+  }
+};
+</script>
+
+<style >
+body {
+  font-size: 25px;
+  user-select: none;
+}
+button {
+  border: none;
+  background: white;
+  font-size: 18px;
+  outline: none;
+}
+</style>
